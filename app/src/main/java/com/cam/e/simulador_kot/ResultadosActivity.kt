b0bce6dccc06448d4cec08765f95fa9659a0518b
Mario Eliezer Cisneros <marioeliezercb@outlook.es>
2018-11-21 23:44:15 -0600

    mañana a las 7

--- app/src/main/java/com/cam/e/simulador_kot/ResultadosActivity.kt
+++ app/src/main/java/com/cam/e/simulador_kot/ResultadosActivity.kt
@@ -1,12 +1,32 @@
 package com.cam.e.simulador_kot
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class ResultadosActivity : AppCompatActivity() {
-
+ private lateinit var  tvResultado:TextView
+    private lateinit var  tvConsejo:TextView
+ private lateinit var bund:Bundle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultados)
+        bund=intent.extras!!
+        tvResultado.text=""+bund.getInt("buenas")+"/"+bund.getInt("todas")
+    }
+    private fun onAceptarResultados(v:View){
+        var intento:Intent= Intent(this,MenuActivity::class.java)
+        startActivity(intento)
+    }
+    private fun aconsejar(){
+
+        if(bund.getInt("buenas")==5)
+            tvConsejo.text="Excelente sigue asi"
+        else if (bund.getInt("buenas") >= 3)
+            tvConsejo.text="Muy bien, pero aun puedes mejorar"
+        else
+            tvConsejo.text="Deberias esforzarte mas....."
     }
 }
