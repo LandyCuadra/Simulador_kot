ffa1dc53f53d1b2960178abc9a0a0f4a36c044b9
Mario Eliezer Cisneros <marioeliezercb@outlook.es>
2018-11-21 21:07:26 -0600

    PROBALO POR FAVOR >:V LAAAANDY ya puse las clases a como sergio :´v

--- app/src/main/java/com/cam/e/simulador_kot/Preguntas.kt
+++ app/src/main/java/com/cam/e/simulador_kot/Preguntas.kt
@@ -1,10 +1,11 @@
 package com.cam.e.simulador_kot
 
-class Preguntas(Enunciado:String,Materia:String,R1:String,R2:String,R3:String,RC:String){
+class Preguntas(Enunciado:String,Materia:String,R1:String,R2:String,R3:String,R4:String,RC:String){
     var Enunciado:String=""
     var materia:String=""
     var R1:String=""
     var R2:String=""
     var R3:String=""
+    var R4:String=""
     var RC:String=""
 }
